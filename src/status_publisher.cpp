9782ce40bb4013df26e84119ce3683d3ed0f3bbb
Aatish <aatish.ayyapath@outlook.com>
2026-02-22 22:29:38 +0530

    Fix timing alignment with grader

--- src/status_publisher.cpp
+++ src/status_publisher.cpp
@@ -1,67 +1,59 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
 #include "rclcpp/rclcpp.hpp"
 #include "ros2_custom_msgs/msg/robot_status.hpp"
-#include <chrono>
 
 using namespace std::chrono_literals;
 
 class StatusPublisher : public rclcpp::Node
 {
 public:
-  StatusPublisher()
-  : Node("status_publisher"),
-    battery_level_(100.0),
-    mission_count_(0),
-    first_run_(true)
-  {
-    publisher_ = this->create_publisher<ros2_custom_msgs::msg::RobotStatus>(
-      "/robot_status", 10);
+    StatusPublisher()
+        : Node("status_publisher"), battery_level_(100.0), mission_count_(0)
+    {
+        publisher_ = this->create_publisher<ros2_custom_msgs::msg::RobotStatus>("/robot_status", 10);
 
-    timer_ = this->create_wall_timer(
-      1000ms,
-      std::bind(&StatusPublisher::timer_callback, this));
-  }
-
-private:
-  void timer_callback()
-  {
-    auto message = ros2_custom_msgs::msg::RobotStatus();
-
-    if (!first_run_) {
-      battery_level_ -= 0.5;
-      mission_count_++;
+        timer_ = this->create_wall_timer(
+            1000ms,
+            std::bind(&StatusPublisher::timer_callback, this));
     }
 
-    message.robot_name = "Explorer1";
-    message.battery_level = battery_level_;
-    message.is_active = true;
-    message.mission_count = mission_count_;
+private:
+    void timer_callback()
+    {
+        auto msg = ros2_custom_msgs::msg::RobotStatus();
 
-    publisher_->publish(message);
+        msg.robot_name = "Explorer1";
+        msg.battery_level = battery_level_;
+        msg.is_active = true;
+        msg.mission_count = mission_count_;
 
-    RCLCPP_INFO(
-      this->get_logger(),
-      "Robot: %s, Battery: %.2f, Active: %s, Missions: %d",
-      message.robot_name.c_str(),
-      message.battery_level,
-      message.is_active ? "true" : "false",
-      message.mission_count
-    );
+        publisher_->publish(msg);
 
-    first_run_ = false;
-  }
+        RCLCPP_INFO(this->get_logger(),
+            "Publishing: robot=%s, battery=%.1f, active=%s, missions=%d",
+            msg.robot_name.c_str(),
+            msg.battery_level,
+            msg.is_active ? "true" : "false",
+            msg.mission_count);
 
-  rclcpp::Publisher<ros2_custom_msgs::msg::RobotStatus>::SharedPtr publisher_;
-  rclcpp::TimerBase::SharedPtr timer_;
+        battery_level_ -= 0.5;
+        mission_count_++;
+    }
 
-  double battery_level_;
-  int mission_count_;
-  bool first_run_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<ros2_custom_msgs::msg::RobotStatus>::SharedPtr publisher_;
+    double battery_level_;
+    int32_t mission_count_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<StatusPublisher>());
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<StatusPublisher>());
+    rclcpp::shutdown();
+    return 0;
 }
